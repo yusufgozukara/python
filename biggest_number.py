be1561b563b644c758340e318b01b3619f16bbdd
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2022-05-17 23:11:30 +0300

    Add files via upload

---
+++ biggest_number.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a another number: "))
+c = int(input("Enter a another number: "))
+
+if (a > b) and (a > c) : 
+  print("First number is bigger")
+elif (b > a) and (b > c) : 
+  print("Second number is bigger")
+else :
+  print("Third number is bigger")

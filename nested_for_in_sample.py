d442e6efb883a01d9a9d4fd3240a858c5450bfed
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2022-06-09 20:15:45 +0300

    Add files via upload

---
+++ nested_for_in_sample.py
@@ -0,0 +1,14 @@
+names = ["susan", "tom", "edward"]
+mood = ["happy", "sad"]
+
+for i in names:
+  for ii in mood:
+    print(i + " is " + ii)
+
+Result : 
+susan is happy
+susan is sad
+tom is happy
+tom is sad
+edward is happy
+edward is sad
